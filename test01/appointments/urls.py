851f54138f1aaaf41bbd9f79ed0c99cedcb001d4
ываппарпвпрвпывыа <stds58@gmail.com>
2024-01-21 16:40:54 +0300

    Initial commit

--- test01/appointments/urls.py
+++ test01/appointments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # Импортируем созданное нами представление
 from .views import AppointmentView
 
-#app_name = "appointmentses"
+#app_name = "appointments"
 urlpatterns = [
    path('', AppointmentView.as_view(), name='make_appointment'),
 
